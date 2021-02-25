e7152e57204802e277a11262a3f5057c441bada5
ian <iansnr10@gmail.com>
2021-02-25 13:26:59 +0300

    findint the square root

--- .idea/codeStyles/ideaprojects.py
+++ .idea/codeStyles/ideaprojects.py
@@ -6,3 +6,9 @@ num2 = input('Enter second number' )
 sum = float(num1) + (num2)
 
 print("the sum of {0} and {1} is {2}".format(num1, num2, sum))
+
+#find the square root
+sum = 8
+
+num_sqrt = num ** 0.5
+print("the square root of %0.3f is %0.3f"%(num,num_sqrt))
